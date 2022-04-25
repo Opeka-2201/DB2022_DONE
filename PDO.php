7b5d0543fc529b6491688cf8bae0e1629ab3b874
Romain Lambermont <lambermontr@gmail.com>
2022-04-25 11:48:37 +0200

    display error ..

--- PDO.php
+++ PDO.php
@@ -1,12 +1,12 @@
 <?php
 try
 {
-  $host = 'ms8db';
+  $host = 'localhost';
   $db = new PDO('mysql:' . $host  . ';dbname=group25;charset=utf8', $_SESSION['user'], $_SESSION['pwd']);
 }
 catch (Exception $e)
 {
   echo $e->getMessage();
-  die('Erreur! ' . $host);
+  die('Erreur! ');
 }
 ?>
