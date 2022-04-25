2afa8206b1972f0be7300dc336fe70e07048d5a6
Opeka-2201 <65912187+Opeka-2201@users.noreply.github.com>
2022-04-25 15:43:32 +0200

    inTransaction test OK

--- PDO.php
+++ PDO.php
@@ -2,11 +2,7 @@
 try
 {
   $host = 'localhost';
-<<<<<<< HEAD
-  $db = new PDO('mysql:' . $host  . ';dbname=group25;charset=utf8', $_SESSION['user'], $_SESSION['pwd']);
-=======
   $db = new PDO('mysql:host=' . $host  . ';dbname=group25;charset=utf8', $_SESSION['user'], $_SESSION['pwd']);
->>>>>>> 5e2a1ad (inTransaction test)
 }
 catch (Exception $e)
 {
